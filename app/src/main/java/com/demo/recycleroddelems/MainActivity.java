012e3e9a8c5ee3c7da845247ece0f22c35691556
Salman Khan <salman@sensibol.com>
2019-05-02 16:01:57 +0530

    Fetched user from repo to activity

--- app/src/main/java/com/demo/recycleroddelems/MainActivity.java
+++ app/src/main/java/com/demo/recycleroddelems/MainActivity.java
@@ -1,14 +1,27 @@
 package com.demo.recycleroddelems;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        UserRepository userRepository = new UserRepository();
+        List<UserRepository.User> users = userRepository.getAllUsers();
+
+        for (UserRepository.User user : users) {
+            Log.d(TAG, String.format("onCreate ID : %d, Name : %s", user.getId(), user.getName()));
+        }
+
     }
 }
